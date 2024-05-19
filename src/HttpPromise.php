3bb7f474e1e478d5def2de6c88e0a0979922a687
omegaalfa <omegaalfa.wr3@gmail.com>
2024-05-19 02:25:58 -0300

    Update HttpPromise.php
    Refatoring class out performance

--- src/HttpPromise.php
+++ src/HttpPromise.php
@@ -4,182 +4,90 @@ declare(strict_types=1);
 
 namespace omegaalfa\HttpPromise;
 
-use Exception;
-use Laminas\Diactoros\Response;
+use CurlHandle;
+use CurlMultiHandle;
+use Psr\Http\Message\ResponseInterface;
 use omegaalfa\HttpPromise\Promise;
 
 
 class HttpPromise
 {
+
 	/**
 	 * array
 	 */
 	private const VALID_HTTP_METHODS = [
-			'GET',
-			'POST',
-			'PUT',
-			'DELETE',
-			'HEAD',
-			'OPTIONS',
-			'TRACE',
-			'CONNECT',
+		'GET',
+		'POST',
+		'PUT',
+		'DELETE',
+		'HEAD',
+		'OPTIONS',
+		'TRACE',
+		'CONNECT',
 	];
 
 	/**
-	 * @var Response
+	 * @var CurlMultiHandle
 	 */
-	protected Response $response;
+	private CurlMultiHandle $multiHandle;
+
 	/**
-	 * @var array
+	 * @var array<int, mixed>
+	 */
+	private array $handles = [];
+
+	/**
+	 * @var list<mixed>
 	 */
 	private array $headers;
 
 	/**
-	 * construct
+	 * @param  ResponseInterface  $response
 	 */
-	public function __construct()
+	public function __construct(protected ResponseInterface $response)
 	{
-		$this->response = new Response();
+		$this->multiHandle = curl_multi_init();
 	}
 
 
 	/**
-	 * @param  string      $method
-	 * @param  string      $url
-	 * @param  mixed|null  $params
-	 * @param  array       $headers
+	 * @param  CurlHandle   $handle
+	 * @param  string       $url
+	 * @param  string       $method
+	 * @param  list<mixed>  $headers
+	 * @param  mixed        $params
 	 *
-	 * @return Promise
+	 * @return void
 	 */
-	public function request(string $method, string $url, mixed $params = null, array $headers = []): Promise
+	private function setCurlOptions(CurlHandle $handle, string $url, string $method, array $headers, mixed $params): void
 	{
-		if(!$this->validMethod($method)) {
-			throw  new \InvalidArgumentException('METHOD REQUEST INVALID');
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($handle, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+
+		if(!empty($headers)) {
+			curl_setopt($handle, CURLOPT_HTTPHEADER, $this->getProcessHeaders($headers));
 		}
 
-		try {
-			$promise = new Promise(function($resolve, $reject) use ($method, $url, $params, $headers) {
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_ENCODING, 'utf-8');
-				curl_setopt($ch, CURLOPT_PROXYPORT, "80");
-				curl_setopt($ch, CURLOPT_CERTINFO, true);
-				curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getProcessHeaders($headers));
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $this->getProcessParams($params, $headers));
-
-				if(is_string($content = curl_exec($ch))) {
-					$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-					$response = $this->response->withStatus($code ?? 200);
-					$response->getBody()->write($content);
-					$response->getBody()->rewind();
-				}
-				$error = curl_error($ch);
-				curl_close($ch);
-
-				if(isset($response)) {
-					$resolve($response);
-				}
-				if($error) {
-					$reject($error);
-				}
-			});
-		} catch(Exception $e) {
-			$promise->reject($e->getMessage());
+		if((is_array($params) || is_object($params)) && !empty($params) && (strtoupper($method) === 'GET')) {
+			$url .= '?' . http_build_query($params);
 		}
 
-		return $promise;
-	}
-
-	/**
-	 * @param  string  $url
-	 * @param  array   $headers
-	 *
-	 * @return Promise
-	 */
-	public function get(string $url, array $headers = []): Promise
-	{
-		try {
-			$promise = new Promise(function($resolve, $reject) use ($url, $headers) {
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
-				curl_setopt($ch, CURLOPT_ENCODING, 'utf-8');
-				curl_setopt($ch, CURLOPT_PROXYPORT, "80");
-				curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getProcessHeaders($headers));
-				if(is_string($content = curl_exec($ch))) {
-					$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-					$response = $this->response->withStatus($code ?? 200);
-					$response->getBody()->write($content);
-					$response->getBody()->rewind();
-				}
-				$error = curl_error($ch);
-				curl_close($ch);
-
-				if(isset($response)) {
-					$resolve($response);
-				}
-				if($error) {
-					$reject($error);
-				}
-			});
-		} catch(Exception $e) {
-			$promise->reject($e->getMessage());
+		if((is_array($params) || is_object($params)) && !empty($params)) {
+			curl_setopt($handle, CURLOPT_POSTFIELDS, $this->getProcessParams($params, $headers));
 		}
 
-		return $promise;
-	}
-
-	/**
-	 * @param  string      $url
-	 * @param  mixed|null  $params
-	 * @param  array       $headers
-	 *
-	 * @return Promise
-	 */
-	public function post(string $url, mixed $params = null, array $headers = []): Promise
-	{
-		return $this->request('POST', $url, $params, $headers);
-	}
-
-	/**
-	 * @param  string      $url
-	 * @param  mixed|null  $params
-	 * @param  array       $headers
-	 *
-	 * @return Promise
-	 */
-	public function put(string $url, mixed $params = null, array $headers = []): Promise
-	{
-		return $this->request('PUT', $url, $params, $headers);
+		curl_setopt($handle, CURLOPT_URL, $url);
 	}
 
 
 	/**
-	 * @param  string      $url
-	 * @param  mixed|null  $params
-	 * @param  array       $headers
+	 * @param  mixed        $params
+	 * @param  list<mixed>  $headers
 	 *
-	 * @return Promise
+	 * @return false|mixed|string
 	 */
-	public function delete(string $url, mixed $params = null, array $headers = []): Promise
-	{
-		return $this->request('DELETE', $url, $params, $headers);
-	}
-
-
-	/**
-	 * @param  mixed       $params
-	 * @param  array|null  $headers
-	 *
-	 * @return bool|mixed|string
-	 */
-	private function getProcessParams(mixed $params = '', array $headers = null): mixed
+	private function getProcessParams(mixed $params, array $headers): mixed
 	{
 		$data = $params;
 
@@ -187,36 +95,28 @@ class HttpPromise
 			$headers['Content-Type'] = 'text/html; charset=utf-8';
 		}
 
-		if(is_array($params) || is_object($params)) {
+		if(is_array($data) || is_object($data)) {
 			$data = urldecode(http_build_query($data));
 		}
 
-		if($params && isset($headers['Content-Type']) && str_contains($headers['Content-Type'], 'json')) {
-			$data = $this->toJson($params);
+		if($params
+			&& isset($headers['Content-Type'])
+			&& is_string($headers['Content-Type'])
+			&& str_contains($headers['Content-Type'], 'json')) {
+			try {
+				$data = json_encode($params, JSON_THROW_ON_ERROR);
+			} catch(\JsonException $e) {
+				throw new \RuntimeException('Error de parametro json: ' . $e->getMessage());
+			}
 		}
 
 		return $data;
 	}
 
-
 	/**
-	 * @param  mixed  $params
+	 * @param  list<mixed>  $headers
 	 *
-	 * @return false|string
-	 */
-	private function toJson(mixed $params): bool|string
-	{
-		try {
-			return json_encode($params, JSON_THROW_ON_ERROR);
-		} catch(\JsonException $e) {
-			return urldecode(http_build_query(['error' => $e->getMessage()]));
-		}
-	}
-
-	/**
-	 * @param  array  $headers
-	 *
-	 * @return array
+	 * @return array<int|string, mixed>
 	 */
 	private function getProcessHeaders(array $headers): array
 	{
@@ -226,13 +126,90 @@ class HttpPromise
 
 		if($headers) {
 			foreach($headers as $name => $value) {
-				$this->headers[] = sprintf('%s: %s', $name, $value);
+				if($value) {
+					$this->headers[] = sprintf('%s: %s', $name, $value);
+				}
 			}
 		}
 
 		return $this->headers;
 	}
 
+	/**
+	 * @param  string       $method
+	 * @param  string       $url
+	 * @param  list<mixed>  $headers
+	 * @param  mixed        $params
+	 *
+	 * @return Promise
+	 */
+	public function request(string $method, string $url, array $headers, mixed $params): Promise
+	{
+
+		$promise = new Promise();
+		$handle = curl_init($url);
+		if(!$handle instanceof CurlHandle) {
+			throw new \RuntimeException('Error: $ch is not instanceof CurlHandle');
+		}
+
+		$this->setCurlOptions($handle, $url, $method, $headers, $params);
+		curl_multi_add_handle($this->multiHandle, $handle);
+
+		$this->handles[(int)$handle] = [
+			'handle'  => $handle,
+			'promise' => $promise
+		];
+
+		return $promise;
+	}
+
+	/**
+	 * @param  string       $url
+	 * @param  list<mixed>  $headers
+	 * @param  mixed|null   $params
+	 *
+	 * @return Promise
+	 */
+	public function get(string $url, array $headers = [], mixed $params = null): Promise
+	{
+		return $this->request('GET', $url, $headers, $params);
+	}
+
+	/**
+	 * @param  string       $url
+	 * @param  list<mixed>  $headers
+	 * @param  mixed|null   $params
+	 *
+	 * @return Promise
+	 */
+	public function post(string $url, mixed $headers = [], mixed $params = null): Promise
+	{
+		return $this->request('POST', $url, headers: $headers, params: $params);
+	}
+
+	/**
+	 * @param  string       $url
+	 * @param  list<mixed>  $headers
+	 * @param  mixed|null   $params
+	 *
+	 * @return Promise
+	 */
+	public function put(string $url, mixed $headers = [], mixed $params = null): Promise
+	{
+		return $this->request('PUT', $url, headers: $headers, params: $params);
+	}
+
+	/**
+	 * @param  string       $url
+	 * @param  list<mixed>  $headers
+	 * @param  mixed|null   $params
+	 *
+	 * @return Promise
+	 */
+	public function delete(string $url, mixed $headers = [], mixed $params = null): Promise
+	{
+		return $this->request('DELETE', $url, headers: $headers, params: $params);
+	}
 
 	/**
 	 * @param  string  $method
@@ -246,5 +223,39 @@ class HttpPromise
 		}
 
 		return false;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function wait(): void
+	{
+		do {
+			$status = curl_multi_exec($this->multiHandle, $active);
+			curl_multi_select($this->multiHandle);
+		} while($active && $status === CURLM_OK);
+
+		while($info = curl_multi_info_read($this->multiHandle)) {
+			$handle = $info['handle'];
+			$handleData = $this->handles[(int)$handle];
+			if(($promise = $handleData['promise']) && $promise instanceof Promise) {
+				if($info['result'] === CURLE_OK) {
+					$response = $this->response;
+					if($code = (int)curl_getinfo($handle, CURLINFO_HTTP_CODE)) {
+						$response = $response->withStatus($code);
+					}
+					if($content = curl_multi_getcontent($handle)) {
+						$response->getBody()->write($content);
+					}
+					$response->getBody()->rewind();
+					$promise->resolve($response);
+				} else {
+					$promise->reject(curl_error($handle));
+				}
+			}
+			curl_multi_remove_handle($this->multiHandle, $handle);
+			curl_close($handle);
+			unset($this->handles[(int)$handle]);
+		}
 	}
 }
